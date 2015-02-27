988d23d564d0362fd140403006fbe5998ae67d33
Shawn Catanzarite <me@shawncatz.com>
2015-02-26 21:30:09 -0800

    get nginx ssl certs working with app

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,7 @@ set :deploy_to, '/srv/apps/ldm'
 
 set :nginx_server_name, 'ldap.rgops.com'
 # ignore this if you do not need SSL
-# set :nginx_use_ssl, true
-# set :nginx_ssl_cert_local_path, '/path/to/ssl_cert.crt'
-# set :nginx_ssl_cert_key_local_path, '/path/to/ssl_cert.key'
+set :nginx_use_ssl, true
+set :nginx_upload_local_cert, false # already installed on server
+set :nginx_ssl_cert, 'wildcard.rgops.com.combined.crt'
+set :nginx_ssl_cert_key, 'wildcard.rgops.com.key'
