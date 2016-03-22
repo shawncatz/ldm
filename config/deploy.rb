3c2708e6d042205dec0a4b9875dccc9aecfe35a0
Shawn Catanzarite <me@shawncatz.com>
2016-03-22 14:37:21 -0700

    use ulive.sh certs

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ set :unicorn_logrotate_enabled, true
 # ignore this if you do not need SSL
 set :nginx_use_ssl, true
 set :nginx_upload_local_cert, false # already installed on server
-set :nginx_ssl_cert, 'wildcard.rgops.com.combined.crt'
-set :nginx_ssl_cert_key, 'wildcard.rgops.com.key'
+set :nginx_ssl_cert, 'wildcard.ulive.sh.combined.crt'
+set :nginx_ssl_cert_key, 'wildcard.ulive.sh.key'
 
 namespace :deploy do
 
